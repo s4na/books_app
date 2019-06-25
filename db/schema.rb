2a5a2b46a3cec9082f7b5d29088658dc845aa3d2
s4na <appletea.umauma@gmail.com>
2019-06-25 17:34:57 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_081751) do
+ActiveRecord::Schema.define(version: 2019_06_25_083336) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
