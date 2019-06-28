f2877ecdef4ba2412f741ea9817d7f14a36513fa
s4na <appletea.umauma@gmail.com>
2019-06-28 15:52:20 +0900

    comment追加用にcontroller設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+
+    p "comment_params" # debug
+    pp comment_params # debug
+    p "@book.comments" # debug
+    pp @book.comments # debug
+
+    @book.comments.create(comment_params)
+    redirect_to book_path(@book)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:name, :memo)
+    end
 end
