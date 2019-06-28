5eba2db649524c44d14445a58628a509e6915f86
s4na <appletea.umauma@gmail.com>
2019-06-28 17:55:27 +0900

    コメントの誤記修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
   def create
     unless params[:book_id].nil?
-      p "params[:book_id] = nil"
+      p "params[:book_id] = not nil" # debug
       @book = Book.find(params[:book_id])
       @book.comments.create(comment_params)
       redirect_to book_path(@book)
     end
 
     unless params[:report_id].nil?
-      p "params[:report_id] = nil"
+      p "params[:report_id] = not nil" # debug
       @report = Report.find(params[:report_id])
       @report.comments.create(comment_params)
       redirect_to report_path(@report)
