6a8a75614d71a4e7e7a18e25dcf2eacaf2bf2814
s4na <appletea.umauma@gmail.com>
2019-06-28 14:49:24 +0900

    commentのmodelをgenerate

---
+++ db/migrate/20190628054847_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.string :name
+      t.text :memo
+      t.references :book, foreign_key: true
+      t.references :report, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
