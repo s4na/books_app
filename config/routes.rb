709fc6e3d2b436143800c15904a701dd34ab571b
s4na <appletea.umauma@gmail.com>
2019-06-28 17:42:51 +0900

    routesにreportsとcommentsの関連付け

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :reports
   resources :books do
+    resources :comments
+  end
+
+  resources :reports do
     resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
