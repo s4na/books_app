ba9879f9ed1ad3ff19499ffbb7355c462105389c
s4na <appletea.umauma@gmail.com>
2019-06-29 17:54:14 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_082240) do
+ActiveRecord::Schema.define(version: 2019_06_29_082325) do
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -23,10 +22,10 @@ ActiveRecord::Schema.define(version: 2019_06_28_082240) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "name"
-    t.text "memo"
     t.string "commentable_type"
     t.integer "commentable_id"
+    t.string "name"
+    t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
@@ -41,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_06_28_082240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
