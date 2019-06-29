0f0f7bed48747e6984349cd3c695713752b00416
s4na <appletea.umauma@gmail.com>
2019-06-29 17:52:57 +0900

    commentのmigration fileのpolymorphic関連付け修正

--- db/migrate/20190629082325_create_comments.rb
+++ db/migrate/20190629082325_create_comments.rb
@@ -1,7 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.reference{polymorphic} :commentable
+      # t.reference{polymorphic} :commentable, index: true
+      t.references :commentable, polymorphic: true, index: true
       t.string :name
       t.text :memo
 
