d1af8fd4cf879dceaa0e96abd1e8c3f1d26c3b46
s4na <appletea.umauma@gmail.com>
2019-06-29 16:29:05 +0900

    comment controllerをベタ書きで動作するように変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,20 @@
 class CommentsController < ApplicationController
   def create
     unless params[:book_id].nil?
-      p "params[:book_id] = not nil" # debug
-      @book = Book.find(params[:book_id])
-      @book.comments.create(comment_params)
-      redirect_to book_path(@book)
+      book = Book.find(params[:book_id])
+      commentable.create(comment_params)
+      comment = book.comments.new(comment_params)
+      comment.save!
+
+      redirect_to book_path(book)
     end
 
     unless params[:report_id].nil?
-      p "params[:report_id] = not nil" # debug
-      @report = Report.find(params[:report_id])
-      @report.comments.create(comment_params)
-      redirect_to report_path(@report)
+      report = Report.find(params[:report_id])
+      comment = report.comments.new(comment_params)
+      comment.save!
+
+      redirect_to report_path(report)
     end
   end
 
