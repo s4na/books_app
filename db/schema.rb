24f3d907f87d8058de58284955e8c7af95444c19
s4na <appletea.umauma@gmail.com>
2019-07-04 15:29:07 +0900

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_06_29_082325) do
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -41,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_06_29_082325) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
