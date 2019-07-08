ec5724db9e7c4603299299d7dc349ccf3624f77a
s4na <appletea.umauma@gmail.com>
2019-07-08 16:45:39 +0900

    rubocop -a

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     I18n.available_locales = [:ja, :en]
     config.i18n.default_locale = :ja
 
