3f7cd4a1a57a894acdb43d4e51909e823a679bd3
s4na <appletea.umauma@gmail.com>
2019-07-09 21:46:49 +0900

    books, reports, commentsにpaginates_perを指定

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,7 +6,7 @@ class ReportsController < ApplicationController
   # GET /reports
   # GET /reports.json
   def index
-    @reports = Report.page(params[:page]).per(5)
+    @reports = Report.page(params[:page])
   end
 
   # GET /reports/1
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,5 @@
 
 class Report < ApplicationRecord
   has_many :comments, as: :commentable
+  paginates_per 5
 end
