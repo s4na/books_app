48626d0c9fbba18b25c22f4970633f7fd4edb116
s4na <appletea.umauma@gmail.com>
2019-07-10 01:18:05 +0900

    save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action only: [:show, :edit, :update, :destroy]
 
   # GET /resource
   # GET /resource.json
@@ -14,7 +15,18 @@ class UsersController < ApplicationController
   # GET "users/:id"
   def show
     p "UsersController#show"
-    @user = User.find(params[:id])
+
+    # p "params[:id]"
+    # pp params[:id]
+
+    # p "User.find_by(id: params[:id]).nil?"
+    # pp User.find_by(id: params[:id])
+
+    # unless User.find_by(id: params[:id]).nil?
+    @user = User.find_by(id: params[:id])
+    # end
+    # p "@user"
+    # pp @user
   end
 
   private
