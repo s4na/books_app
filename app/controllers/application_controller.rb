901e94d440a5b5ee9fb36b961398846fb461de82
s4na <appletea.umauma@gmail.com>
2019-07-10 01:18:05 +0900

    application_cにuser/name情報追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :street_address, :self_introduction])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :street_address, :self_introduction])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :street_address, :self_introduction])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :street_address, :self_introduction])
     end
 end
