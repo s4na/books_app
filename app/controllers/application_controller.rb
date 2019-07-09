62f1433293006b28d56acbc02ccac468bc1860b5
s4na <appletea.umauma@gmail.com>
2019-07-10 01:18:05 +0900

    application_cにsign_up, account_updateのpermit追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
     { locale: I18n.locale }
@@ -11,4 +12,10 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :street_address, :self_introduction])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :street_address, :self_introduction])
+    end
 end
