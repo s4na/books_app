11df7185b0d297190c65a6db1b2ede90ec539fda
s4na <appletea.umauma@gmail.com>
2019-07-12 15:15:51 +0900

    使用していない関数削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action only: [:show, :edit, :update, :destroy]
 
   # GET /resource
   # GET /resource.json
@@ -14,9 +14,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
-
-  private
-    def set_user
-      @user = User.find(params[:id])
-    end
 end
