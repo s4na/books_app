adc3210825e930ef85c492b068fb1d5d7e137a27
s4na <appletea.umauma@gmail.com>
2019-07-12 15:09:22 +0900

    無効になっていた関数を復活

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  # before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /resource
   # GET /resource.json
