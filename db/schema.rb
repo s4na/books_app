387e9afd4250b19bab5cf131bc6559406dccc2fd
s4na <appletea.umauma@gmail.com>
2019-07-17 18:07:45 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_073718) do
+ActiveRecord::Schema.define(version: 2019_07_12_070237) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -48,13 +48,17 @@ ActiveRecord::Schema.define(version: 2019_07_06_073718) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
+    t.string "name", default: "", null: false
     t.string "postal_code"
     t.string "street_address"
     t.string "self_introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider", default: "", null: false
+    t.string "uid", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 end
