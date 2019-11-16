e00aa212b641c243dfb6be774d0f1fd6cae396a0
s4na <appletea.umauma@gmail.com>
2019-11-17 03:50:33 +0900

    一時テスト環境に戻す

--- config/puma.rb
+++ config/puma.rb
@@ -35,4 +35,4 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
